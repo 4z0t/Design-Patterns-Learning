a121b81a74957d8fa623bfe49df930313fbe66fb
Alexander <84857900+4z0t@users.noreply.github.com>
2023-03-02 11:02:51 +0300

    Добавьте файлы проекта.

---
+++ DesignPatterns/BoardBus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    internal class BoardBus : IBoardAnyCar
+    {
+        public void BoardDriver(IDriver driver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BoardPassanger(Passanger passanger)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/BoardTaxi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    internal class BoardTaxi : IBoardAnyCar
+    {
+        public void BoardDriver(IDriver driver)
+        {
+            throw new NotImplementedException();
+        }
+        
+        void BoardPassanger(Passanger passanger)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
