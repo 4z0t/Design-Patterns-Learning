825ae9f47dd238c7edd3a7708aa2fc2e309136ee
Alexander <84857900+4z0t@users.noreply.github.com>
2023-03-04 12:02:09 +0300

    add Example1

---
+++ DesignPatterns/Example1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    internal class Example1
+    {
+
+        private static Passanger[] MakePassangers(int n)
+        {
+            Passanger[] passangers = new Passanger[n];
+            for (int i = 0; i < n; i++)
+            {
+                passangers[i] = new Passanger();
+            }
+            return passangers;
+        }
+
+        public static void Start()
+        {
+            PassangerLoader busLoader = new PassangerLoader(new BusFactory());
+
+            Passanger[] passangers = MakePassangers(40);
+
+            for (int i = 0; i < passangers.Length; i++)
+            {
+                if (!busLoader.LoadPassanger(passangers[i])) break;
+                Console.WriteLine("Loaded passenger " + i.ToString());
+            }
+
+
+            PassangerLoader  taxiLoader = new PassangerLoader(new TaxiFactory());
+
+            Passanger[] taxiPassangers = MakePassangers(10);
+
+            for (int i = 0; i < taxiPassangers.Length; i++)
+            {
+                if (!taxiLoader.LoadPassanger(taxiPassangers[i])) break;
+                Console.WriteLine("Loaded passenger " + i.ToString());
+            }
+
+        }
+
+    }
+}
