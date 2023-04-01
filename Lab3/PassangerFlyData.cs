74ffcc4db42a3144c74f2f3f2993a9a55eb08ed0
Alexander <84857900+4z0t@users.noreply.github.com>
2023-04-01 13:35:19 +0300

    add PassangerFlyData

---
+++ Lab3/PassangerFlyData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public enum BaggageStatus
+    {
+        ACCEPTED,
+        REMOVED
+    }
+    public class PassangerFlyData
+    {
+        public float surcharge;
+        public Passenger passenger;
+        public BaggageStatus status;
+
+        public PassangerFlyData(float surcharge, Passenger passenger, BaggageStatus baggageStatus)
+        {
+            this.surcharge = surcharge;
+            this.passenger = passenger;
+            this.status = baggageStatus;
+
+        }
+    }
+}
---
+++ Lab3/Plane.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Plane
+    {
+        public People Pilots { get; } = new PilotsCrew();
+        public People Stewardesses { get; } = new StewardessesCrew();
+        public FlyClass FirstClass { get; } = new FirstClass();
+        public FlyClass BusinessClass { get; } = new BusinessClass();
+        public FlyClass EconomyClass  { get; } = new EconomyClass();
+
+        private float _maxWeight;
+
+        public Plane(float maxWeight)
+        {
+            _maxWeight = maxWeight;
+        }
+
+        public void Load()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
