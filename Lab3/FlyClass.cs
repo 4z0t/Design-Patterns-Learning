a4d6001147156ff438d751ce9fd567b1f1e6b8b1
Alexander <84857900+4z0t@users.noreply.github.com>
2023-04-01 12:15:43 +0300

    add fly classes

---
+++ Lab3/FlyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+
+
+    public abstract class FlyClass : People
+    {
+        public abstract int PassangersCount { get; }
+
+        public FlyClass()
+        {
+            var random = new Random(PassangersCount);
+            for (int i = 0; i < PassangersCount; i++)
+            {
+                Add(new Passenger(random.Next(5, 65)));
+            }
+        }
+
+    }
+}
---
+++ Lab3/FlyClasses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class FirstClass : FlyClass
+    {
+        public override int PassangersCount => 10;
+    }
+
+    public class BusynessClass : FlyClass
+    {
+        public override int PassangersCount => 20;
+    }
+
+    public class EconomyClass : FlyClass
+    {
+        public override int PassangersCount => 150;
+    }
+}
