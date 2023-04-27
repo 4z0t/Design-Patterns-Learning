aa5519bf4819d54e23f9acee86b8bfd881e6e927
Alexander <84857900+4z0t@users.noreply.github.com>
2023-04-27 11:23:22 +0300

    add QuotesCorrectionExpression

---
+++ Lab5/QuotesCorrectionExpression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class QuotesCorrectionExpression : IExpression
+    {
+        public void Interpret(Context context)
+        {
+            context.input = Regex.Replace(context.input, "“(.)”", "«$1»");
+        }
+    }
+}
